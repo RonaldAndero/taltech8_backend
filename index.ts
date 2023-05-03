9c2703789ed2a77883740c9329e561439f56460b
Ronald Andero Õisman <ronald-andero.oisman@khk.ee>
2023-05-03 16:54:55 +0300

    Installs cors

--- index.ts
+++ index.ts
@@ -2,9 +2,12 @@ import express, { Express, Request, Response } from "express";
 import stringsController from "./controllers/strings";
 import productsController from "./controllers/products";
 import productlistController from "./controllers/productlist";
+import cors from "cors";
+
 
 const app: Express = express();
 
+app.use(cors())
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
 });
